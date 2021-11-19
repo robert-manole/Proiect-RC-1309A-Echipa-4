f7a3cc09dde8b01c588b3d4587c9f43f51f1e761
laurversanu <laurversanu78@gmail.com>
2021-11-19 11:40:49 +0200

    second

--- Client.py
+++ Client.py
@@ -1,5 +1,7 @@
 import socket
 import Packets as packets
+import struct
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 class Client:
@@ -8,9 +10,24 @@ class Client:
         self.__id = id
 
     def connect(self):
-        s.connect(('127.0.0.1', 5000))
+        s.connect(('127.0.0.1', 6000))
 
         packet = packets.CONNECT()
+        packet = packet.parse()
+
+        s.sendall(packet)
+        msg = s.recv(1024)
+        print(msg)
+
+    def publish(self):
+        packet = packets.PUBLISH()
+        packet = packet.parse()
+
+        s.sendall(packet)
+        msg = s.recv(1024)
+        print(msg)
+    def subscribe(self):
+        packet = packets.SUBSCRIBE()
         packet = packet.parse()
 
         s.sendall(packet)
@@ -20,4 +37,7 @@ class Client:
 
 client = Client(1)
 client.connect()
+client.publish()
+client.subscribe()
+
 
--- Packets.py
+++ Packets.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 packetFixedHeader = {
     'CONNECT': b'\x10',
-    'PUBLISH': b'\x30',
+    'PUBLISH': b'\x32',
     'SUBSCRIBE': b'\x82',
     'UNSUBSCRIBE': b'\xA2',
     'PINGREQ': b'\xC0',
@@ -12,7 +12,6 @@ class Packet(ABC):
     @abstractmethod
     def parse(self):
         pass
-
 
 class CONNECT(Packet):
     packetPayload = {
@@ -53,10 +52,66 @@ class CONNECT(Packet):
         return packet
 
 class PUBLISH(Packet):
-    pass
+    packetPayload = {
+        'message': "Hello"
+    }
+    packetVariableHeader = {
+        'topic_name': "First",
+        'packetIdentifier': b'\x00\x0a'
+    }
+
+    def parse(self):
+        packet = bytearray()
+
+        packet += packetFixedHeader['PUBLISH']
+
+        variable_header = b'\x00'
+        variable_header += bytes([len(self.packetVariableHeader['topic_name'])])
+        variable_header += self.packetVariableHeader['topic_name'].encode('UTF-8')
+        variable_header += self.packetVariableHeader['packetIdentifier']
+
+        payload = b'\x00'
+        payload += bytes([len(self.packetPayload['message'])])
+        payload += self.packetPayload['message'].encode('UTF-8')
+        variable_header += payload
+
+        packet_length = bytes([len(variable_header)])
+        packet += packet_length
+        packet += variable_header
+
+        return packet
+
+
 
 class SUBSCRIBE(Packet):
-    pass
+    packetPayload = {
+        'Topic_name': "Hello",
+        'req_QoS': b'\x01'
+    }
+    packetVariableHeader = {
+        'packetIdentifier': b'\x00\x0b'
+    }
+
+    def parse(self):
+        packet = bytearray()
+
+        packet += packetFixedHeader['SUBSCRIBE']
+
+        # variable_header = b'\x00'
+        variable_header = self.packetVariableHeader['packetIdentifier']
+
+        payload = b'\x00'
+        payload += bytes([len(self.packetPayload['Topic_name'])])
+        payload += self.packetPayload['Topic_name'].encode('UTF-8')
+        payload += self.packetPayload['req_QoS']
+
+        variable_header += payload
+
+        packet_length = bytes([len(variable_header)])
+        packet += packet_length
+        packet += variable_header
+
+        return packet
 
 class UNSUBSCRIBE(Packet):
     pass
